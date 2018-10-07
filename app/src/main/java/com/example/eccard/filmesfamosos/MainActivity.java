5b0981f171065e01b1f75fa8b82eb7f5c31e2f0a
eccard <felipelugao@id.uff.br>
2018-10-07 11:14:39 -0300

    Add RecyclerView and menu

--- app/src/main/java/com/example/eccard/filmesfamosos/MainActivity.java
+++ app/src/main/java/com/example/eccard/filmesfamosos/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.eccard.filmesfamosos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int GRID_COLLUMS = 2;
+
+    @BindView(R.id.rv_movies)
+    RecyclerView mRecycleView;
+
+    @BindView(R.id.pb_main)
+    ProgressBar mPB;
+
+    @BindView(R.id.tv_generic_error)
+    TextView mTvGenericError;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if ( item.getItemId() == R.id.action_refresh){
+            getData();
+            return true;
+        }else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
