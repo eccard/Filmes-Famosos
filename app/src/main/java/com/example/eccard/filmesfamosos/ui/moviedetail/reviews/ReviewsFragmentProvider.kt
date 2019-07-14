6300daf833756202103d2ff4e1114aa106a5a36b
eccard <felipelugao@id.uff.br>
2019-07-14 11:03:49 -0300

    Improves ui.moviedatail main codes
    Refactor to kotlin, add MVVM arquitecture, use dataBinding

---
+++ app/src/main/java/com/example/eccard/filmesfamosos/ui/moviedetail/reviews/ReviewsFragmentProvider.kt
@@ -0,0 +1,10 @@
+package com.example.eccard.filmesfamosos.ui.moviedetail.reviews
+
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+internal abstract class ReviewsFragmentProvider{
+    @ContributesAndroidInjector
+    internal abstract fun provideTrailerFragmentFactory(): FrgReviews
+}
